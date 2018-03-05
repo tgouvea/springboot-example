57a1b180fd41ec6b86fc848f4c02732b2c04a9df
oi404118 <tiago.gouvea@oi.net.br>
2018-03-05 18:06:28 -0300

    exemplo de trabalho com profiles

--- src/main/java/com/tiagogouvea/api/SpringbootExampleApplication.java
+++ src/main/java/com/tiagogouvea/api/SpringbootExampleApplication.java
@@ -1,13 +1,27 @@
 package com.tiagogouvea.api;
 
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
 public class SpringbootExampleApplication {
+	
+	@Value("${paginacao.qtd_por_pagina}")
+	private int qtPaginas;
 
 	public static void main(String[] args) {
 		SpringApplication.run(SpringbootExampleApplication.class, args);
 		System.out.println("Teste Travis");
+	}
+	
+	@Bean
+	public CommandLineRunner teste() {
+		
+		return args -> {
+			System.out.println("Quantidade = " + this.qtPaginas); 
+		};
 	}
 }
